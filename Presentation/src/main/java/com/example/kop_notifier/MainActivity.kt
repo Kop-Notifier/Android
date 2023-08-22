8962192d5a581014cbd30810c842dee6b536730a
oh in seong <superohinsung@naver.com>
2023-08-22 21:03:08 +0900

    Compose setting

--- Presentation/src/main/java/com/example/kop_notifier/MainActivity.kt
+++ Presentation/src/main/java/com/example/kop_notifier/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.kop_notifier
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.kop_notifier.ui.theme.Kop_NotifierTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Kop_NotifierTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
     }
 }
---
+++ Presentation/src/main/java/com/example/kop_notifier/MainScreen.kt
@@ -0,0 +1,23 @@
+package com.example.kop_notifier
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kop_notifier.ui.theme.Kop_NotifierTheme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Kop_NotifierTheme {
+        Greeting("Android")
+    }
+}
