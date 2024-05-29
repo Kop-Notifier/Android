82891520ae09090ce21c7a3306c37a754969d9a3
oh in seong <58154638+ois0886@users.noreply.github.com>
2024-05-29 17:49:19 +0900

    feat : Compose Setting

--- presentation/src/main/java/com/example/android_part/MainActivity.kt
+++ presentation/src/main/java/com/example/android_part/MainActivity.kt
@@ -1,17 +1,47 @@
 package com.example.android_part
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.android_part.databinding.ActivityMainBinding
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android_part.ui.theme.Android_PartTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Android_PartTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_PartTheme {
+        Greeting("Android")
     }
 }
